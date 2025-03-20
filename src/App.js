6efd151884e3c5fd3cfec793df668521fef8ee02
Sambhaji patil <patilsambha11@gmail.com>
2025-03-20 20:58:50 +0530

    added user table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './layout/Navbar';
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-   <Navbar/>
+      <Navbar />
+      <Home />
     </div>
   );
 }
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">f-stack application</a>
     <button 
       className="navbar-toggler"
       type="button"
@@ -13,8 +13,10 @@ export default function Navbar() {
       aria-controls="navbarSupportedContent" 
       aria-expanded="false"
       aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <span className="navbar-toggler-icon"></span>hiiiii
     </button>
+
+    <button className='btn  btn-outline-light '>Add User</button>
    
   </div>
 </nav>
